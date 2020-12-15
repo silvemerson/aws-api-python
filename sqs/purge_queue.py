df011b8020c320ae3aa48fc0dd813595bc81e8e6
silvemerson <araujo.emerson28@gmail.com>
2020-12-15 20:41:17 -0300

    update

--- sqs/purge_queue.py
+++ sqs/purge_queue.py
@@ -4,15 +4,17 @@ client = boto3.client('sqs', region_name='us-east-1')
 
 
 try:
-
         response = client.purge_queue(
              QueueUrl= input('Insira a fila que será feito o purge: ')
         )
 
         print("Purge feito com sucesso")
 
-except:
-        print('Fila não encontrada')
+except client.exceptions.QueueDoesNotExist as error :
+        print(error)
+
+except client.exceptions.PurgeQueueInProgress as error :
+        print(error)
 
 
 
